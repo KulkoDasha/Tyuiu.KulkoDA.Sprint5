5f57e3afb7ebf0d88d1183190afdc0fe04e13566
Tyuiu.KulkoDA@outlook.com <Tyuiu.KulkoDA@outlook.com>
2024-11-23 21:47:47 +0500

    Task1

---
+++ Tyuiu.KulkoDA.Sprint5.Task1.V4.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.KulkoDA.Sprint5.Task1.V4.Lib;
+namespace Tyuiu.KulkoDA.Sprint5.Task1.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Даша\source\repos\Tyuiu.KulkoDA.Sprint5\Tyuiu.KulkoDA.Sprint5.Task1.V4\bin\Debug\OutPutFileTask1.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExist = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExist);
+        }
+    }
+}
---
+++ Tyuiu.KulkoDA.Sprint5.Task1.V4/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.KulkoDA.Sprint5.Task1.V4.Lib;
+namespace Tyuiu.KulkoDA.Sprint5.Task1.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Кулько Д. А. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнила: Кулько Дарья Андреевна | ИИПб-24-2                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана функция, на диапазоне[-5;5] с шагом 1.                             *");
+            
+            Console.WriteLine("*                                                                         *");
+            int a = -5;
+            int b = 5;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("StartValue = " + a);
+            Console.WriteLine("StotValue = " + b);
+            string res = ds.SaveToFileTextData(a,b);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Файл: " + res + " Создан!");
+            Console.ReadKey();
+        }
+    }
+}
