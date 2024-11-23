9e8f5b867b1fb0f03f8421698b9c3d2bd2070fc0
Tyuiu.KulkoDA@outlook.com <Tyuiu.KulkoDA@outlook.com>
2024-11-23 20:59:32 +0500

    Task0

--- Tyuiu.KulkoDA.Sprint5.Task0.V27.Lib/DataService.cs
+++ Tyuiu.KulkoDA.Sprint5.Task0.V27.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.KulkoDA.Sprint5.Task0.V27.Lib
         public string SaveToFileTextData(int x)
         {
             string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
-            double y = Math.Round(Math.Pow((x - 1), (3*x-1)),3);
+            double y = Math.Pow((x - 1), (3*x+1));
             File.WriteAllText(path, Convert.ToString(x));
             return path;
         }
