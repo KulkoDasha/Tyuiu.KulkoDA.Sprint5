d378ddf340c60989be162a9eea2449ce882469fa
Tyuiu.KulkoDA@outlook.com <Tyuiu.KulkoDA@outlook.com>
2024-11-28 23:16:50 +0500

    2222

--- Tyuiu.KulkoDA.Sprint5.Task7.V10.Lib/DataService.cs
+++ Tyuiu.KulkoDA.Sprint5.Task7.V10.Lib/DataService.cs
@@ -25,10 +25,12 @@ namespace Tyuiu.KulkoDA.Sprint5.Task7.V10.Lib
                     {
                         for (int i = 0; i < line.Length; i++)
                         {
-                            if ((line[i] == line.ToUpper()[i])&((int)ch >= 97 && (int)ch <= 122))
+                            if ((line[i] == line.ToUpper()[i]) & ((int)ch >= 97 && (int)ch <= 122))
                             {
                                 line = line.Replace(line[i], line.ToLower()[i]);
                             }
+                            else
+                                return line;
                         }
                     }
                     
